0bbbd24f710aa38742b3902d042286ba9420216c
mohaimin37 <137748062+mohaimin37@users.noreply.github.com>
2024-06-03 12:39:31 +0530

    Add files via upload

---
+++ server/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const data = ()=>{
+    mongoose.connect("mongodb+srv://syedanas:anas@food.cr9jq2n.mongodb.net/").then(()=>{
+        console.log("your database base has been connected")
+    }).catch((err)=>{
+        console.log("error data base has not been connected " + err )
+    
+    })
+}
+data()
+
+   
+module.exports = data
+
+
+
