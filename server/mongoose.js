cb74eeb71a43131d96491cd9a6479bb158e04838
mohaimin37 <137748062+mohaimin37@users.noreply.github.com>
2024-06-03 12:46:02 +0530

    Update mongoose.js

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose")
-const data = ()=>{
     mongoose.connect("mongodb+srv://syedanas:anas@food.cr9jq2n.mongodb.net/").then(()=>{
         console.log("your database base has been connected")
     }).catch((err)=>{
         console.log("error data base has not been connected " + err )
     
     })
-}
-data()
 
    
 module.exports = data
